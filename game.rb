6d7b3b3a49c0173aa06136644f34d7c628525a63
Tiago <tvhollowknight@gmail.com>
2024-11-23 16:20:32 -0300

    Changed the style of the printed board
    Board in game now looks more apealing

--- game.rb
+++ game.rb
@@ -1,25 +1,38 @@
 require_relative 'check_winner'
 class Game
   include CheckVictory
-  @@board_layout = [%w[- - -], %w[- - -], %w[- - -]]
+  @@board_pieces = [%w[- - -], %w[- - -], %w[- - -]]
   @@plays = 0
   def initialize(row, column, piece)
     @row = row
     @column = column
     @piece = piece
-    return if @@board_layout[@row][@column] != '-'
+    return if @@board_pieces[@row][@column] != '-'
 
-    @@board_layout[@row][@column] = @piece
+    @@board_pieces[@row][@column] = @piece
     @@plays += 1
   end
 
-  def self.board_layout
-    @@board_layout
+  def self.board_pieces
+    @@board_pieces
   end
 
   def self.winner(board)
     return false if @@plays < 3
 
     CheckVictory.find_winner(board)
+  end
+
+  def self.print_board(pieces)
+    board_layout = "     |     |
+  #{pieces[0][0]}  |  #{pieces[0][1]}  |  #{pieces[0][2]}
+_____|_____|_____
+     |     |
+  #{pieces[1][0]}  |  #{pieces[1][1]}  |  #{pieces[1][2]}
+_____|_____|_____
+     |     |
+  #{pieces[2][0]}  |  #{pieces[2][1]}  |  #{pieces[2][2]}
+     |     |     "
+    puts board_layout
   end
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,7 @@
 require_relative 'game'
 winner_found = false
 until winner_found != false
-  puts(Game.board_layout.map { |row| row.join(' ') })
+  Game.print_board(Game.board_pieces)
 
   x_choice = [0, 1]
   o_choice = nil
@@ -9,14 +9,14 @@ until winner_found != false
     print 'Where will X play: '
     x_choice = gets.chomp.split('').map(&:to_i)
     break if (x_choice.length == 2) && (x_choice[0].to_i in (0..2)) && (x_choice[1].to_i in (0..2)) &&
-             Game.board_layout[x_choice[0]][x_choice[1]] == '-'
+             Game.board_pieces[x_choice[0]][x_choice[1]] == '-'
   end
   Game.new(x_choice[0].to_i, x_choice[1].to_i, 'x')
-  winner_found = Game.winner(Game.board_layout)
-  puts(Game.board_layout.map { |row| row.join(' ') })
+  winner_found = Game.winner(Game.board_pieces)
+  Game.print_board(Game.board_pieces)
   break unless winner_found == false
 
-  if Game.board_layout.flatten.include?('-') == false
+  if Game.board_pieces.flatten.include?('-') == false
     winner_found = 'No one'
     break
   end
@@ -25,12 +25,12 @@ until winner_found != false
     print 'Where will O play: '
     o_choice = gets.chomp.split('').map(&:to_i)
     break if (o_choice.length == 2) && (o_choice[0].to_i in (0..2)) && (o_choice[1].to_i in (0..2)) &&
-             Game.board_layout[o_choice[0]][o_choice[1]] == '-'
+             Game.board_pieces[o_choice[0]][o_choice[1]] == '-'
   end
   Game.new(o_choice[0].to_i, o_choice[1].to_i, 'o')
-  winner_found = Game.winner(Game.board_layout)
+  winner_found = Game.winner(Game.board_pieces)
   unless winner_found == false
-    puts(Game.board_layout.map { |row| row.join(' ') })
+    Game.print_board(Game.board_pieces)
     break
   end
 end
